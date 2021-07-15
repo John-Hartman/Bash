cedfabc2e11079c584cdc60395db6b1774e203c4
John hartman <57684113+John-Hartman@users.noreply.github.com>
2021-07-15 12:25:14 -0400

    Update pre-patching.sh

--- QRadar/pre-patching.sh
+++ QRadar/pre-patching.sh
@@ -8,17 +8,20 @@ dir=/store/LOGS/QRadar_Patching_$d
 echo "Working on the pre-patching script"
 
 
+echo "Removing the junk you forgot to clean up"
 #removes old deployment_info outputs
 rm -rf /store/LOGS/qradar_deploymet_info*
 
-echo "Removing the junk you forgot to clean up"
 
+echo "Creating patch files directory"
 #Create directory for patching documents
 mkdir $dir
 
+echo "Checking the space on all managed hosts"
 # Verify you have enough space in the QRadar console
 /opt/qradar/support/all_servers.sh -C "df -h" > $dir/diskcheck.txt
 
+echo "Running the drq health check"
 # health check
 /opt/qradar/support/all_servers.sh -C "drq" > $dir/drq_output.txt
 
@@ -26,9 +29,16 @@ mkdir $dir
 /opt/qradar/support/deployment_info.sh 
 mv /store/LOGS/qradar_deployment_info* $dir
 
+echo "Checking the current version of all appliances"
 # Confirm all appliances are at the same version
 /opt/qradar/support/all_servers.sh -C "/opt/qradar/bin/myver" > $dir/myver_output.txt
 
+echo "Creating the updates directory on all managed hosts"
 /opt/qradar/support/all_servers.sh -C "mkdir -p /media/updates" > /dev/null
+
+
+echo "Copying the sfs file to all managed hosts"
+#Copy the file down to all managed hosts:
+/opt/qradar/support/all_servers.sh -P /store/*.sfs
 
 echo "$dir created with all of your files, have a wonderful day ya filthy animals"
